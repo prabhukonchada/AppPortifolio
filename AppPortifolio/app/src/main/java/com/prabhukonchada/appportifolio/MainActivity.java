f6e967ae1e691479c890198aa3bb660c7e4d9309
Prabhu Konchada <prabhukonchada@summerinterns>
2016-05-26 10:26:19 +0530

    Added header text view to the activity and initialised the component

---
+++ AppPortifolio/app/src/main/java/com/prabhukonchada/appportifolio/MainActivity.java
@@ -0,0 +1,75 @@
+package com.prabhukonchada.appportifolio;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+/**
+ * Created by Prabhu Konchada on 25/03/16
+ * for FanStation
+ * you can contact me at : prabhukonchada@gmail.com
+ * git lab link for more details on the project : https://github.com/prabhukonchada/AppPortifolio
+ * or visit www.prabhukonchada.com
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView headerTitle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // To initialise components used in the activity
+        initComponents();
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /***
+     * Initialize components  used in the activity
+     */
+    public void initComponents()
+    {
+        headerTitle = (TextView)findViewById(R.id.headerText);
+    }
+
+}
