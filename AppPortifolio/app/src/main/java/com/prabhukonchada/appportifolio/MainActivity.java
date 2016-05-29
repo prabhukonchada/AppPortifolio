4ec183ce5e1cba6f57554cb7fa990f5d5fbb85a8
Prabhu Konchada <prabhukonchada@summerinterns>
2016-05-29 16:06:47 +0530

    Code improvements after peer review

--- AppPortifolio/app/src/main/java/com/prabhukonchada/appportifolio/MainActivity.java
+++ AppPortifolio/app/src/main/java/com/prabhukonchada/appportifolio/MainActivity.java
@@ -3,6 +3,7 @@ package com.prabhukonchada.appportifolio;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -17,99 +18,43 @@ import android.widget.Toast;
  */
 
 public class MainActivity extends AppCompatActivity {
-
-    TextView headerTitle;
-    Button openPopularMoviesAppBtn;
-    Button openStockHawkAppBtn;
-    Button openBuildItBiggerAppBtn;
-    Button openMakeYourAppMaterialAppBtn;
-    Button openGoUbiquitousAppBtn;
-    Button openCapstoneAppBtn;
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        // To initialise components used in the activity
-        initComponents();
-
-        // Set component titles programatically
-        setComponentTitlesProgramatically();
     }
 
     /***
-     * Initialize components  used in the activity
-     */
-    public void initComponents()
-    {
-        headerTitle = (TextView)findViewById(R.id.headerText);
-        openPopularMoviesAppBtn = (Button)findViewById(R.id.popularMoviesRedirectionButton);
-        openBuildItBiggerAppBtn = (Button)findViewById(R.id.buildItBiggerRedirectionButton);
-        openStockHawkAppBtn = (Button)findViewById(R.id.stockHawkRedirectionButton);
-        openGoUbiquitousAppBtn = (Button)findViewById(R.id.goUbiquitousRedirectionButton);
-        openCapstoneAppBtn = (Button)findViewById(R.id.capstoneRedirectionButton);
-        openMakeYourAppMaterialAppBtn = (Button)findViewById(R.id.makeYourAppMaterialRedirectionButton);
-    }
-
-    /***
-     * Set titles programatically for the layout elements.
-     */
-    public void setComponentTitlesProgramatically()
-    {
-        headerTitle.setText(R.string.app_header);
-        openPopularMoviesAppBtn.setText(R.string.app_popular_movies_button);
-        openBuildItBiggerAppBtn.setText(R.string.app_build_it_bigger);
-        openStockHawkAppBtn.setText(R.string.app_stock_hawk);
-        openGoUbiquitousAppBtn.setText(R.string.app_ubiquitous);
-        openMakeYourAppMaterialAppBtn.setText(R.string.app_make_it_material);
-        openCapstoneAppBtn.setText(R.string.app_capstone);
-    }
-
-    /***
-     * The following methods with open*() would redirect to the appropriate app once developed.
+     * The following method openApplication() would redirect to the appropriate app once developed.
      * At present it displays a toast message appropriately.
-     * @param contentView The view where action is defined is passed automatically
+     * @param contentView Based on the view passed the redirection takes place.
      */
-    public void openPopularMoviesApp(View contentView)
+    public void openApplication(View contentView)
     {
-        Toast.makeText(this, returnTextForToast(getResources().getString(R.string.app_popular_movies_button)), Toast.LENGTH_LONG).show();
+        switch (contentView.getId())
+        {
+            case R.id.popularMoviesRedirectionButton:
+                Toast.makeText(this, R.string.app_popular_movies, Toast.LENGTH_LONG).show();
+                break;
+            case R.id.buildItBiggerRedirectionButton:
+                Toast.makeText(this, R.string.app_build_it_bigger, Toast.LENGTH_LONG).show();
+                break;
+            case R.id.makeYourAppMaterialRedirectionButton:
+                Toast.makeText(this, R.string.app_make_it_material, Toast.LENGTH_LONG).show();
+                break;
+            case R.id.stockHawkRedirectionButton:
+                Toast.makeText(this, R.string.app_stock_hawk, Toast.LENGTH_LONG).show();
+                break;
+            case R.id.goUbiquitousRedirectionButton:
+                Toast.makeText(this, R.string.app_ubiquitous, Toast.LENGTH_LONG).show();
+                break;
+            case R.id.capstoneRedirectionButton:
+                Toast.makeText(this, R.string.app_capstone, Toast.LENGTH_LONG).show();
+                break;
+        }
     }
 
-    public void openStockHawkApp(View contentView)
-    {
-        Toast.makeText(this, returnTextForToast(getResources().getString(R.string.app_stock_hawk)), Toast.LENGTH_LONG).show();
-    }
-
-    public void openGoUbiquitousApp(View contentView)
-    {
-        Toast.makeText(this, returnTextForToast(getResources().getString(R.string.app_ubiquitous)), Toast.LENGTH_LONG).show();
-    }
-
-    public void openBuildItBiggerApp(View contentView)
-    {
-        Toast.makeText(this, returnTextForToast(getResources().getString(R.string.app_build_it_bigger)), Toast.LENGTH_LONG).show();
-    }
-
-    public void openMakeYourAppMaterialApp(View contentView)
-    {
-        Toast.makeText(this, returnTextForToast(getResources().getString(R.string.app_make_it_material)), Toast.LENGTH_LONG).show();
-    }
-
-    public void openCapstoneApp(View contentView)
-    {
-        Toast.makeText(this, returnTextForToast(getResources().getString(R.string.app_capstone)), Toast.LENGTH_LONG).show();
-    }
-
-    /***
-     * This method used to form string message
-     * @param appName Application name that is to be launched
-     * @return Toast Message
-     */
-    public String returnTextForToast(String appName)
-    {
-        return ("This button will launch my ".concat(appName).concat(" App"));
-    }
 }
